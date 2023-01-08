ecdb14023293ebdbe732cb528cbed52a32c88d8d
Joel Antony <joel.geoanto@gmail.com>
2023-01-08 11:37:08 +0530

    made changes

--- lib/input.dart
+++ lib/input.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+const activeColour=Color(0xFF1D1E33);
+const inactiveColour=Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,11 +9,41 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleColour=inactiveColour;
+  Color femaleColour=inactiveColour;
+
+  void changeColour(int gender) {
+    //gender=1 for male and gender=2 for female
+    if (gender == 1) {
+      femaleColour=inactiveColour;
+      if (maleColour == inactiveColour) {
+        maleColour = activeColour;
+      }
+
+      else {
+        maleColour = inactiveColour;
+      }
+    }
+
+    else if (gender==2){
+      maleColour=inactiveColour;
+      if(femaleColour==inactiveColour){
+        femaleColour=activeColour;
+      }
+      else{
+        femaleColour=inactiveColour;
+      }
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('BMI CALCULATOR')
+          title: const Text('BMI CALCULATOR'),
+          centerTitle: true,
         ),
 
         body:Column(
@@ -18,48 +51,109 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: Row(
                     children:[
-                      Expanded(child: ReusableCard(colour:const Color(0xFF1D1E33))),
-                      Expanded(child: ReusableCard(colour:const Color(0xFF1D1E33))
-                      ]
+                      Expanded(
+                        child:
+                        GestureDetector(
+                          onTap:(){
+                            setState(() {
+                              changeColour(1);
+                            });
+                          },
+                          child: Container(
+                            margin:const EdgeInsets.all(15),
+                             decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color:maleColour
+                            ),
+
+                             child:Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(FontAwesomeIcons.mars,size: 80.0),
+                                SizedBox(height: 15.0),
+                                Text('Male',style: TextStyle(fontSize: 18.0,color: Color(0xFF8D8E98)))
+                              ],
+                            ),
+                          ),
+                        )
+                      ),
+
+                      Expanded(child: 
+
+                        GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              changeColour(2);
+                            });
+                          },
+
+                          child: Container(
+                            margin:const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color:femaleColour
+                            ),
+
+                            child:Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(FontAwesomeIcons.venus,size: 80.0),
+                              SizedBox(height: 15.0),
+                              Text('Female',style: TextStyle(fontSize: 18.0,color: Color(0xFF8D8E98)))
+                            ]
+                            )
+                          ),
+                        )
+                      ),
+                    ]
                   ),
                 ),
 
-               Expanded(
-                child: ReusableCard(colour:const Color(0xFF1D1E33))
+             Expanded(
+                child:  Container(
+                  margin:const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color:const Color(0xFF1D1E33)
+                ),
+                )
               ),
               Expanded(child: Row(
                   children:[
-                    Expanded(child: ReusableCard(colour:const Color(0xFF1D1E33))),
-                    Expanded(child: ReusableCard(colour:const Color(0xFF1D1E33)))
+                    Expanded(
+                      child:  Container(
+                        margin:const EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color:const Color(0xFF1D1E33)
+                        ),
+                      )  
+                  ),
+                  
+                    Expanded(
+                      child:  Container(
+                        margin:const EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color:const Color(0xFF1D1E33)
+                        ),
+                      )
+                    ),
+
                   ]
+                ),
               ),
-              ),
+              Container(
+                color:const Color(0xFFEB1555),
+                margin:const EdgeInsets.only(top:10.0),
+                width:double.infinity,
+                height: 80.0,
+              )
             ]
-        )
-    );
+          )
+        );
+
+
   }
 }
-
-class ReusableCard extends StatelessWidget {
-  /*const ReusableCard({
-    Key? key,
-  }) : super(key: key); */ //here the key is used to keep track of any change that can happen during the screen
-
-  ReusableCard({required this.colour,this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin:const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color:const Color(0xFF1D1E33)
-        ),
-      );
-  }
-}
-
 
